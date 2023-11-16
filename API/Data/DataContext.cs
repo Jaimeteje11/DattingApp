7d541ecbcca3b5172951bb0fe78531212e058ee0
Jaime Tejedor Martin <jtejedom@emeal.nttdata.com>
2023-11-16 13:33:45 +0100

    End o s2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users {get; set;} //users es el nombre de nuestra tabla y refenciamos a la clase appuser para que coja los datos de ahi
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    public int Id {get; set;} //columnas de nuestras tablas que seran primary keys lo puedes hacer tambien con [Key]
+    public string UserName {get;set;} //columnas de nuestras tablas que seran primary keys
+}
